065ed03ed6eb5d2229db45bd0cfc4942de31864b
dinga99 <aecifuentesv@gmail.com>
2016-05-15 21:34:39 -0400

    gooogleeeeeee...

--- backapp/auth.js
+++ backapp/auth.js
@@ -1,6 +1,6 @@
 module.exports = {
     'googleAuth':{
-        'clientID'      : '286244984750-4189cm2noo9ukebvk12960a8i7c1i87o.apps.googleusercontent.com ',
+        'clientID'      : '286244984750-4189cm2noo9ukebvk12960a8i7c1i87o.apps.googleusercontent.com',
         'clientSecret': 'C4ku1GTgtk2a32FBtBCAi1mu',
         'callbackURL'   : 'https://examprepcycle.herokuapp.com/authcallback'
     }
