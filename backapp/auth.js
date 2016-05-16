1940a16962339e8179c3ca992e2571990548d26b
dinga99 <aecifuentesv@gmail.com>
2016-05-15 20:45:36 -0400

    3rd upload

--- backapp/auth.js
+++ backapp/auth.js
@@ -2,6 +2,6 @@ module.exports = {
     'googleAuth':{
         'clientID'      : '286244984750-aghdrvsa365bgo8rgm6rn4je245e08je.apps.googleusercontent.com',
         'clientSecret': 'cJkSTpx3_tT_ywidIgD8gvfV',
-        'callbackURL'   : 'http://localhost:8080/authcallback'
+        'callbackURL'   : 'http://examprepcycle.herokuapp.com/authcallback'
     }
 }
